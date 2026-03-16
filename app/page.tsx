b5d2239690ee49f2863dba376200e125af67568b
JesusCharrisPo <pedrazajuanse288@gmail.com>
2026-03-16 06:59:28 +0100

    JesusCharrisPo

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import { Header } from "@/components/header"
 import { Hero } from "@/components/hero"
 import { About } from "@/components/about"
-import { ComoPoedoAyudarte } from "@/components/ComoPoedoAyudarte"
+import { Services } from "@/components/services"
 import { GalleryDesarrolloWeb } from "@/components/gallery-desarrollo-web"
 import { GalleryAudiovisual } from "@/components/gallery-audiovisual"
 import { AIVideoShowcase } from "@/components/ai-video-showcase"
 import { GalleryCatalogoRopa } from "@/components/gallery-catalogo-ropa"
+import { BeforeAfterIA } from "@/components/BeforeAfterIA"
 import { Blog } from "@/components/blog"
 import ContactForm from "@/components/ContactForm"
 import { Contact } from "@/components/contact"
@@ -17,16 +18,22 @@ export default function Page() {
       <Header />
       <Hero />
       <About />
-      <ComoPoedoAyudarte />
+      <Services />
       <GalleryDesarrolloWeb />
       <GalleryAudiovisual />
+
       {/* Nueva sección IA */}
       <AIVideoShowcase />
+
       <GalleryCatalogoRopa />
+
       {/* PORTFOLIO OCULTO */}
+
       <Blog />
+
       {/* FORMULARIO */}
       <ContactForm />
+
       <Contact />
       <Footer />
     </main>

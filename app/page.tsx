4740dadd9e3e670129fb92e500a92c94eaa50c03
JesusCharrisPo <pedrazajuanse288@gmail.com>
2026-03-16 07:14:07 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -10,22 +10,24 @@ import { Blog } from "@/components/blog"
 import ContactForm from "@/components/ContactForm"
 import { Contact } from "@/components/contact"
 import { Footer } from "@/components/footer"
-import { ShowreelHero }   from "@/components/ShowreelHero"
-import { StatsSection }   from "@/components/StatsSection"
+import { ShowreelHero } from "@/components/ShowreelHero"
+import { StatsSection } from "@/components/StatsSection"
 import { ProcesoCreativo } from "@/components/ProcesoCreativo"
 
 export default function Page() {
   return (
     <main>
       <Header />
-      <ShowreelHero />   {/* ← justo después del Header, reemplaza o complementa el Hero actual */}
+      <ShowreelHero />
       <Hero />
       <About />
-      <StatsSection />   {/* ← después del About, genera confianza */}
+      <StatsSection />
       <Services />
+      <GalleryDesarrolloWeb />
       <GalleryAudiovisual />
-      <BeforeAfterIA />
-      <ProcesoCreativo /> {/* ← antes del contacto, cierra el funnel */}
+      <AIVideoShowcase />
+      <GalleryCatalogoRopa />
+      <ProcesoCreativo />
       <Blog />
       <ContactForm />
       <Contact />
